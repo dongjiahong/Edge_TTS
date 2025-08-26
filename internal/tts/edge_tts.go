ace36602dac124a46983a51b87643ebee78b0b50
jiahong.dong <dongjiahong@hotmail.com>
2025-08-26 11:18:22 +0800

    update

--- internal/tts/edge_tts.go
+++ internal/tts/edge_tts.go
@@ -66,12 +66,10 @@ func (c *EdgeTTSClient) connect() (*websocket.Conn, error) {
 	headers.Set("Accept-Language", "en-US,en;q=0.9")
 	headers.Set("Cache-Control", "no-cache")
 	headers.Set("Pragma", "no-cache")
-	headers.Set("Sec-WebSocket-Extensions", "permessage-deflate; client_max_window_bits")
-	headers.Set("Sec-WebSocket-Version", "13")
 
 	dialer := websocket.Dialer{
 		HandshakeTimeout: 30 * time.Second,
-		Subprotocols:     []string{},
+		EnableCompression: true,
 	}
 
 	conn, _, err := dialer.Dial(c.config.Endpoint, headers)
